c4a4fba6ea22ba41a6421371c2dcbbc8fe575479
goodmorningdata <goodmorningdata@gmail.com>
2019-03-17 15:21:04 -0700

    Add documentation to nps_create_master_df.py and some code reorganization

--- nps_create_master_df.py
+++ nps_create_master_df.py
@@ -18,8 +18,9 @@ Dependencies:
     * Run the script, nps_create_park_lookup.py, to create the file,
       'nps_park_lookup.xls'
     * Download the most recent acreage report from the nps website at:
-      https://www.nps.gov/subjects/lwcf/acreagereports.htm Place this
-      file in the acreage_data directory of this project.
+      https://www.nps.gov/subjects/lwcf/acreagereports.htm Calendar
+      Year Reports, Year = 2018. Place this file in the acreage_data
+      directory of this project.
     * Download the most recent visitation report from the nps webiste
       at: https://irma.nps.gov/Stats/SSRSReports/National%20Reports/
       Annual%20Visitation%20By%20Park%20(1979%20-%20Last%20Calendar
@@ -105,27 +106,44 @@ def strip_park_lookup(df):
 
     return df
 
-# Park acreage data pulled from:
-# National Park Service Acreage Reports
-# https://www.nps.gov/subjects/lwcf/acreagereports.htm
-# Calendar Year Reports, Year = 2018
 def read_acreage_data(df_parks_lookup):
-    infile = 'acreage_data/NPS-Acreage-12-31-2018.xlsx'
-    cols = ['Area Name', 'State', 'Region', 'Gross Area Acres']
+    '''
+    Read the park acreage data file.
 
+    This function reads the acreage data report into a dataframe,
+    removes rows for parks that are not available through the NPS API,
+    and simplifies the park name so that it can be more easily found in
+    the park lookup table. This function also assigns the correct four-
+    character park code to each row.
+
+    To-Do:
+    - Why are some parks in this list not in the API list? Are they
+      part of a larger park and should be rolled in?
+
+    Parameters
+    ----------
+    df_parks_lookup : Pandas dataframe
+      Park lookup dataframe.
+
+    Returns
+    -------
+    df : Pandas dataframe
+      Park acreage dataframe.
+    '''
+
+    infile = 'acreage_data/NPS-Acreage-12-31-2018.xlsx'
     df = pd.read_excel(infile, skiprows=1)
     df = df[pd.notnull(df['State'])]
+    df = df.rename({'Gross Area Acres': 'gross_area_acres'}, axis='columns')
 
-    # The following park sites are removed because they are not on the
-    # NPS master list. MORE RESEARCH - there are part of other parks?
+    # Remove parks not available through the NPS API park list.
     missing_list = ['R REAGAN BOYHOOD HOME NHS', 'ROSS LAKE NRA',
                     'FT CAROLINE NMEM', 'WHITE HOUSE', 'WORLD WAR I NMEM',
                     'LAKE CHELAN NRA', 'JDROCKEFELLER MEM PKWY']
     df = df[~df['Area Name'].isin(missing_list)]
 
-    # Strip out park designations and abbreviations so that matching to the nps
-    # lookup table will work correctly. Also includes a few text swaps so that
-    # lookup will work.
+    # Strip out park designations, and make a few text replacements so
+    # that park code lookup will be able to find the matching park.
     df['area_name_stripped'] = df['Area Name'].replace({'NBP':'', 'NHP':'',
                                'NHS':'', 'NMEM':'', 'NMP':'', 'NRA':'',
                                'NSR':'', 'NST':'', 'NB':'', 'NL':'', 'NM':'',
@@ -140,39 +158,89 @@ def read_acreage_data(df_parks_lookup):
                                'DELAWARE NSR':'LOWER DELAWARE',
                                'KINGS CANYON':'SEQUOIA & KINGS CANYON'}, regex=True)
 
+    # Call the lookup_park_code function to find the correct four-char
+    # park code for each park row.
     df['park_code'] = df.area_name_stripped.apply(
                       lambda x: lookup_park_code(x, df_parks_lookup))
-    df = df.rename({'Gross Area Acres': 'gross_area_acres'}, axis='columns')
+
     df = df[['park_code', 'gross_area_acres']]
+
+    # Convert gross acres to numeric and total for each park. This is
+    # necessary because some parks have more than one row in the
+    # acreage data file. For example: 'GLACIER BAY NP', and 'GLACIER
+    # BAY N PRESERVE'.
+    df['gross_area_acres'] = pd.to_numeric(df_acre['gross_area_acres'],
+                                           errors='coerce')
+    df = df.groupby(['park_code'], as_index=False).sum()
 
     return df
 
 def lookup_park_code(lookup_park_name, lookup_df):
+    '''
+    Lookup the park code using park name.
+
+    This function finds the row matching the lookup_park_name parameter
+    in the lookup_df park dataframe using SequenceMatcher, and returns
+    the four-character park code of the closest match.
+
+    Parameters
+    ----------
+    lookup_park_name : str
+      Park name to look up.
+    lookup_df : Pandas dataframe
+      Lookup table dataframe.
+
+    Returns
+    -------
+    df : Pandas dataframe
+      Park acreage dataframe.
+    '''
+
     df = lookup_df[['park_name', 'park_code']]
     df['name_match'] = df['park_name'].apply(
                        lambda x: SequenceMatcher(None, x.lower(),
                        lookup_park_name.lower()).ratio())
 
-    #print(df.loc[df['name_match'].idxmax()].park_name)
-    #print(df.loc[df['name_match'].idxmax()].park_code)
-
     return df.loc[df['name_match'].idxmax()].park_code
 
-# Visitor data pulled from - https://irma.nps.gov/Stats/SSRSReports/National%20
-# Reports/Annual%20Visitation%20By%20Park%20(1979%20-%20Last%20Calendar%20Year)
 def read_visitor_data(df_parks_lookup):
+    '''
+    Read the park visitation data file.
+
+    This function reads the park visitor data report into a dataframe,
+    removes rows for parks that are not available through the NPS API,
+    and makes some replacements to the park name string so that it can
+    be more easily found in the park lookup table. This function also
+    assigns the correct four-character park code to each row.
+
+    To-Do:
+    - Why are some parks in this list not in the API list? Are they
+      part of a larger park and should be rolled in?
+
+    Parameters
+    ----------
+    df_parks_lookup : Pandas dataframe
+      Park lookup dataframe.
+
+    Returns
+    -------
+    df : Pandas dataframe
+      Park visitation dataframe.
+    '''
+
     df = pd.read_excel(
          'visitation_data/annual_visitation_by_park_2008_2017.xlsx',
          header=8)
     df = df[['Park Name', 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]]
     df.rename(columns = {'Park Name':'park_name'}, inplace=True)
 
-    # The following park sites are removed from the visitor dataframe because
-    # they are not on the NPS master list. Maybe they are a part of other parks?
+    # Remove parks not available through the NPS API park list.
     missing_list = ['Ross Lake NRA', 'Fort Caroline NMEM', 'Lake Chelan NRA',
                     'John D. Rockefeller, Jr. MEM PKWY']
     df = df[~df['park_name'].isin(missing_list)]
 
+    # Make a few text replacements so that park code lookup will be able
+    # to find the matching park.
     df.park_name.replace(
         {'National Capital Parks Central':'National Mall and Memorial Parks',
          'Longfellow NHS':"Longfellow House Washington's Headquarters",
@@ -181,26 +249,34 @@ def read_visitor_data(df_parks_lookup):
 
     df['park_code'] = df.park_name.apply(
                          lambda x: lookup_park_code(x, df_parks_lookup))
-    #df.set_index(['park_code'], inplace=True)
+
+    # Group and sum visitor rows assigned the same park code.
+    print ('Checking group by by visitor data:')
+    print(df.shape[0])
+    print('** duplicates:')
+    print(df[df.duplicated(['park_code'], keep=False)])
+
+    df = df.groupby(['park_code'], as_index=False).sum()
+    print(df.shape[0])
 
     return df
 
-def read_wikipedia_data():
-    df = pd.read_excel('20181225_wikipedia_list_of_national_parks.xlsx',
-                       header=0,
-                       names=['Name', 'Location', 'State', 'Date Established',
-                              'Area (2017)', 'Visitors (2017)', 'Description'])
-    df.rename(columns = {'Name':'park_name',
-                         'Location':'location',
-                         'State':'state',
-                         'Date Established':'date_established',
-                         'Area (2017)':'area_2017'}, inplace=True)
-    df['park_name'] = df.park_name.apply(lambda x: x.replace('*','').strip())
-    df['park_code'] = df.park_name.apply(lambda x: lookup_park_code(x))
-    df.drop(columns=['park_name'], inplace=True)
-    df.set_index(df.park_code, inplace=True)
-
-    return df
+# def read_wikipedia_data():
+#     df = pd.read_excel('20181225_wikipedia_list_of_national_parks.xlsx',
+#                        header=0,
+#                        names=['Name', 'Location', 'State', 'Date Established',
+#                               'Area (2017)', 'Visitors (2017)', 'Description'])
+#     df.rename(columns = {'Name':'park_name',
+#                          'Location':'location',
+#                          'State':'state',
+#                          'Date Established':'date_established',
+#                          'Area (2017)':'area_2017'}, inplace=True)
+#     df['park_name'] = df.park_name.apply(lambda x: x.replace('*','').strip())
+#     df['park_code'] = df.park_name.apply(lambda x: lookup_park_code(x))
+#     df.drop(columns=['park_name'], inplace=True)
+#     df.set_index(df.park_code, inplace=True)
+#
+#     return df
 
 def main():
     pd.set_option('display.max_rows', 1000)
@@ -209,15 +285,9 @@ def main():
     df_master_stripped = strip_park_lookup(df_master)
 
     df_acre = read_acreage_data(df_master_stripped)
-    df_acre['gross_area_acres'] = pd.to_numeric(df_acre['gross_area_acres'], errors='coerce')
-    df_acre = df_acre.groupby(['park_code'], as_index=False).sum()
-
     df_master = pd.merge(df_master, df_acre, how='left', on='park_code')
-    print(df_master[['park_code', 'designation']])
 
     df_visitor = read_visitor_data(df_master_stripped)
-    df_visitor = df_visitor.groupby(['park_code'], as_index=False).sum()
-
     df_master = pd.merge(df_master, df_visitor, how='left', on='park_code')
 
     df_master.designation.fillna('Other', inplace=True)
