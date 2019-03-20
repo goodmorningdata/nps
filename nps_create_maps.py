14b7610c16af994f7de02251599ec0d5cf6f03cc
goodmorningdata <goodmorningdata@gmail.com>
2019-03-20 08:23:27 -0700

    Add comments and docstrings to nps_create_maps.

--- nps_create_maps.py
+++ nps_create_maps.py
@@ -13,13 +13,31 @@ Dependencies:
 
 This script contains the following functions:
 
-    *
+    * create_map :
+    * add_map_location :
+
 '''
 
 import pandas as pd
 import folium
 
 def create_map():
+    '''
+    Creates a Folium map.
+
+    This function creates a Folium map object, centered on the lat/long
+    center of the lower 48 states.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    map : Folium map object
+      Folium map ready for park location addition.
+    '''
+
     center_lower_48 = [39.833333, -98.583333]
     map = folium.Map(location = center_lower_48,
                      zoom_start = 4,
@@ -28,14 +46,43 @@ def create_map():
                      tiles = 'Stamen Terrain')
     return map
 
-def add_map_location(map, popup, lat, long, icon, color):
+def add_map_location(map, lat, long, icon, color, popup):
+    '''
+    Adds locations to the Folium map object.
+
+    This function adds park locations to the Folium map at the latitude
+    and longitude specified in the parameters. Locations will be
+    marked with an icon (using Font Awesome icons:
+    https://fontawesome.com/icons?d=gallery&m=free), and an icon color.
+    The icon will show a popup message on click with clickable link.
+
+    Parameters
+    ----------
+    map : Folium map object
+      Map on which to plot the location.
+    lat : float
+      Latitude of location to plot.
+    long : float
+      Longitude of location to plot.
+    icon : str
+      Font Awesome icon to mark location.
+    color : str
+      Icon color.
+    popup : str
+      String of html to add link to popup.
+
+    Returns
+    -------
+    map : Folium map object
+      Map object with new location added.
+    '''
+
     popup_html = folium.Html(popup, script=True)
-        marker = folium.Marker(location = [lat, long],
+    marker = folium.Marker(location = [lat, long],
                            icon = folium.Icon(color=color,
                                             prefix='fa',                        icon=icon),
                            popup = folium.Popup(popup_html)
-                           )
-    marker.add_to(map)
+                           ).add_to(map)
 
     return map
 
@@ -43,7 +90,7 @@ def main():
     # Read in the park master dataframe.
     df = pd.read_excel('nps_parks_master_df.xlsx', header=0)
 
-    # Assign map icons and colors to subsets
+    # Assign map icons and colors to park sets.
     icon_df = pd.DataFrame(
               {'park_set' : ['National Park', 'National Monument',
                              'National Preserve or Reserve',
@@ -73,16 +120,17 @@ def main():
         map_df = df[df.park_set == map_park_set]
     else: map_df = df
 
+    # Add each location in the park set dataframe to the map.
     for _, row in map_df[~map_df.lat.isnull()].iterrows():
         icon_df_row = icon_df[icon_df.park_set == row.park_set]
         popup_string = ('<a href="'
                         + 'https://www.nps.gov/' + row.park_code
                         + '" target="_blank">'
                         + row.park_name + '</a>').replace("'", r"\'")
-        park_map = add_map_location(park_map, popup_string,
-                                    row.lat, row.long,
+        park_map = add_map_location(park_map, row.lat, row.long,
                                     icon_df_row.values[0][2],
-                                    icon_df_row.values[0][1])
+                                    icon_df_row.values[0][1],
+                                    popup_string)
 
     park_map.save('nps_parks_map.html')
 
