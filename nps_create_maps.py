b7c7264ccc539208534f085f93aa223b10f17d10
goodmorningdata <goodmorningdata@gmail.com>
2019-03-24 09:53:30 -0700

    Reorganize map code.

--- nps_create_maps.py
+++ nps_create_maps.py
@@ -49,51 +49,7 @@ def create_map():
                      tiles = 'Stamen Terrain')
     return map
 
-def add_map_location(map, lat, long, icon, color, popup):
-    '''
-    Adds locations to the Folium map object.
-
-    This function adds park locations to the Folium map at the latitude
-    and longitude specified in the parameters. Locations will be
-    marked with an icon (using Font Awesome icons:
-    https://fontawesome.com/icons?d=gallery&m=free), and an icon color.
-    The icon will show a popup message on click with clickable link.
-
-    Parameters
-    ----------
-    map : Folium map object
-      Map on which to plot the location.
-    lat : float
-      Latitude of location to plot.
-    long : float
-      Longitude of location to plot.
-    icon : str
-      Font Awesome icon to mark location.
-    color : str
-      Icon color.
-    popup : str
-      String of html to add link to popup.
-
-    Returns
-    -------
-    map : Folium map object
-      Map object with new location added.
-    '''
-
-    popup_html = folium.Html(popup, script=True)
-    marker = folium.Marker(location = [lat, long],
-                           icon = folium.Icon(color=color,
-                                              prefix='fa',
-                                              icon=icon),
-                           popup = folium.Popup(popup_html)
-                           ).add_to(map)
-
-    return map
-
-def main():
-    # Read in the park master dataframe.
-    df = pd.read_excel('nps_parks_master_df.xlsx', header=0)
-
+def add_park_locations_to_map(map, df):
     # Assign map icons and colors to park sets.
     icon_df = pd.DataFrame(
               {'park_set' : ['National Park', 'National Monument',
@@ -110,8 +66,84 @@ def main():
                 'icon' : ['tree', 'tree', 'pagelines', 'tint', 'tint',
                           'pagelines', 'university', 'university', 'pagelines',
                           'road', 'university', 'map-marker', 'map-marker']
-              }
-    )
+              })
+
+    for _, row in map_df[~map_df.lat.isnull()].iterrows():
+
+        # Create popup with link to park website.
+        popup_string = ('<a href="'
+                        + 'https://www.nps.gov/' + row.park_code
+                        + '" target="_blank">'
+                        + row.park_name + '</a>').replace("'", r"\'")
+        popup_html = folium.Html(popup_string, script=True)
+
+        # Assign color and graphic to icon.
+        icon_df_row = icon_df[icon_df.park_set == row.park_set]
+        map_icon = folium.Icon(color=icon_df_row.values[0][1],
+                               prefix='fa',
+                               icon=icon_df_row.values[0][2]),
+        marker = folium.Marker(location = [rwo.lat, row.long],
+                               icon = map_icon
+                               popup = folium.Popup(popup_html)
+                              ).add_to(map)
+
+    return map
+
+def add_park_area_circles_to_map(map, df):
+            # If command-line option specifies a park area map, add each
+            # park to the map with a circle approximating its size.
+    for _, row in df[~df.lat.isnull()].iterrows():
+        tooltip = (row.park_name.replace("'", r"\'")
+                   + ', {:,.0f}'.format(row.gross_area_square_miles)
+                   + ' square miles')
+        folium.Circle(
+            radius=math.sqrt(row.gross_area_square_meters/math.pi),
+            location=[row.lat, row.long],
+            tooltip=tooltip,
+            color='crimson',
+            fill=True,
+            fill_color='crimson'
+        ).add_to(map)
+
+    return map
+
+# def add_map_location(map, lat, long, icon, color, popup):
+#     '''
+#     Adds locations to the Folium map object.
+#
+#     This function adds park locations to the Folium map at the latitude
+#     and longitude specified in the parameters. Locations will be
+#     marked with an icon (using Font Awesome icons:
+#     https://fontawesome.com/icons?d=gallery&m=free), and an icon color.
+#     The icon will show a popup message on click with clickable link.
+#
+#     Parameters
+#     ----------
+#     map : Folium map object
+#       Map on which to plot the location.
+#     lat : float
+#       Latitude of location to plot.
+#     long : float
+#       Longitude of location to plot.
+#     icon : str
+#       Font Awesome icon to mark location.
+#     color : str
+#       Icon color.
+#     popup : str
+#       String of html to add link to popup.
+#
+#     Returns
+#     -------
+#     map : Folium map object
+#       Map object with new location added.
+#     '''
+#
+#
+#     return map
+
+def main():
+    # Read in the park master dataframe.
+    df = pd.read_excel('nps_parks_master_df.xlsx', header=0)
 
     # The user can specify the set of parks to map using the command
     # line parameter, parkset. If no parameter specified, all park sites
@@ -136,40 +168,29 @@ def main():
         map_df = df[df.park_set == args.parkset]
     else: map_df = df
 
+    # Create an empty map
     park_map = create_map()
 
     if args.maptype and args.maptype in ['area','acreage']:
-        # If command-line option specifies a park area map, add each
-        # park to the map with a circle approximating its size.
-        #folium.Popup(row.park_name.replace("'", r"\'"))
-        for _, row in map_df[~map_df.lat.isnull()].iterrows():
-            tooltip = (row.park_name.replace("'", r"\'")
-                       + ', {:,.0f}'.format(row.gross_area_square_miles)
-                       + ' square miles')
-            folium.Circle(
-                radius=math.sqrt(row.gross_area_square_meters/math.pi),
-                location=[row.lat, row.long],
-                tooltip=tooltip,
-                color='crimson',
-                fill=True,
-                fill_color='crimson'
-            ).add_to(park_map)
+        park_map = add_park_area_circles_to_map(park_map, map_df)
         park_map.save('nps_parks_map_area.html')
-        map_df[['park_code', 'park_name', 'gross_area_acres', 'gross_area_square_miles']].sort_values(by=['gross_area_acres']).to_excel('nps_parks_sorted_by_size.xlsx', index=False)
+
+        # Export a sorted list of parks and their size to both an Excel
+        # file and an html file for reference.
+        map_df_export = (map_df[['park_code', 'park_name',
+                                 'gross_area_acres', 'gross_area_square_miles']]
+                         .sort_values(by=['gross_area_acres'], ascending=False)
+                         .reset_index(drop=True))
+        map_df_export.index += 1
+        map_df_export.to_excel('nps_parks_sorted_by_size.xlsx', index=False)
+        map_df_export.to_html('nps_parks_sorted_by_size.html',
+                              justify='left',
+                              float_format=lambda x: '{:,.2f}'.format(x))
+
     else:
         # If command-line option not specified or is type = 'location'
         # or 'loc', add each location in the park set df to the map.
-        for _, row in map_df[~map_df.lat.isnull()].iterrows():
-            icon_df_row = icon_df[icon_df.park_set == row.park_set]
-            popup_string = ('<a href="'
-                            + 'https://www.nps.gov/' + row.park_code
-                            + '" target="_blank">'
-                            + row.park_name + '</a>').replace("'", r"\'")
-            park_map = add_map_location(park_map, row.lat, row.long,
-                                        icon_df_row.values[0][2],
-                                        icon_df_row.values[0][1],
-                                        popup_string)
-
+        park_map = add_park_locations_to_map(park_map, map_df)
         park_map.save('nps_parks_map_location.html')
 
 if __name__ == '__main__':
