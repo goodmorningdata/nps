777f9d92d1b0273ccc43b2ca414395332534edbc
goodmorningdata <goodmorningdata@gmail.com>
2019-05-08 09:05:04 -0700

    Updates to make data reading process cleaner and more accurate.

--- nps_create_master_df.py
+++ nps_create_master_df.py
@@ -45,22 +45,22 @@ def strip_park_name(park_name):
     park_name : str : Stripped park name.
     '''
 
-    designation_start = park_name.lower().find('national')
-    if designation_start > 0:
-        park_name = park_name[:designation_start]
-
-    designation_start = park_name.lower().find('memorial')
-    if designation_start > 0:
-        park_name = (park_name.replace('Memorial', '')
-                              .replace('MEMORIAL', ''))
-
-    park_name = (park_name.replace('N RVR & RA','')
-                          .replace('N REC RIVER','')
-                          .replace('N. SCENIC RIVER','')
-                          .replace('N PRESERVE','')
-                          .replace('NS RIVERWAYS','')
-                          .replace('NM of America','')
-                          .replace('Ecological & Historic Preserve',''))
+    # designation_start = park_name.lower().find('national')
+    # if designation_start > 0:
+    #     park_name = park_name[:designation_start]
+    #
+    # designation_start = park_name.lower().find('memorial')
+    # if designation_start > 0:
+    #     park_name = (park_name.replace('Memorial', '')
+    #                           .replace('MEMORIAL', ''))
+    #
+    # park_name = (park_name.replace('N RVR & RA','')
+    #                       .replace('N REC RIVER','')
+    #                       .replace('N. SCENIC RIVER','')
+    #                       .replace('N PRESERVE','')
+    #                       .replace('NS RIVERWAYS','')
+    #                       .replace('NM of America','')
+    #                       .replace('Ecological & Historic Preserve',''))
 
     return park_name.rstrip()
 
@@ -83,8 +83,26 @@ def read_park_sites_api():
     filename = '_reference_data/nps_park_sites_api.xlsx'
     df = pd.read_excel(filename, header=0)
 
+    exclude_park_codes = ['afam', 'alka', 'anch', 'alca', 'aleu', 'amme',
+                          'anac', 'armo', 'attr', 'auca', 'balt', 'bawa',
+                          'blrn', 'cali', 'crha', 'came', 'cahi', 'cajo',
+                          'chva', 'cbpo', 'cbgn', 'cwdw', 'coal', 'colt',
+                          'xrds', 'dabe', 'dele', 'elte', 'elca', 'elis',
+                          'erie', 'esse', 'fati', 'fodu', 'fofo', 'glec',
+                          'glde', 'grfa', 'grsp', 'guge', 'haha', 'jame',
+                          'hurv', 'inup', 'iafl', 'iatr', 'blac', 'jthg',
+                          'juba', 'keaq', 'klse', 'lecl', 'lode', 'loea',
+                          'maac', 'mide', 'migu', 'mihi', 'mopi', 'auto',
+                          'mush', 'avia', 'npnh', 'neen', 'pine', 'nifa',
+                          'noco', 'oire', 'okci', 'olsp', 'oreg', 'ovvi', 'oxhi', 'para', 'poex', 'prsf', 'rist', 'roca',
+                          'safe', 'scrv', 'semo', 'shvb', 'soca', 'tecw',
+                          'qush', 'thco', 'tosy', 'trte', 'waro', 'whee',
+                          'wing', 'york']
+    print(df.shape)
+    df = df[~(df.park_name in exclude_park_codes)]
     df['park_name_stripped'] = df.park_name.apply(
                                lambda x: strip_park_name(x))
+    print(df.shape)
 
     return df[['park_code', 'park_name', 'park_name_stripped',
                'states', 'lat', 'long']]
@@ -151,35 +169,35 @@ def lookup_park_code(park_name, df_lookup):
     # The park names for the following parks differ so greatly from the
     # name to match to or are so similar to another park name, that they
     # must be assigned directly.
-    if park_name.lower() == "arlington house": park_code = 'arho'
-    if park_name.lower() in ["kings canyon", "sequoia"]: park_code = 'seki'
-    if park_name.lower() == "white house" : park_code = 'whho'
-    if park_name.lower() == "fdr": park_code = 'frde'
-    if park_name.lower() == "gateway nra": park_code = 'gate'
-    if park_name.lower() == "g w": park_code = 'gwmp'
-    if park_name.lower() == "martin l king, jr, nhp": park_code = 'malu'
-    if park_name.lower() == "martin luther king, jr. nhp": park_code = 'malu'
-    if park_name.lower() == "t roosevelt np": park_code = 'thro'
-    if park_name.lower().startswith("fred-spots"): park_code = 'frsp'
-    if park_name.lower().startswith("lbj"): park_code = 'lyba'
-    if park_name.lower().endswith("wwii"): park_code = 'wwii'
-    if park_name.lower().find("sumter") > -1: park_code = 'fosu'
-    if park_name.lower().find("longfellow") > -1: park_code = 'long'
-
-    # These are the parks with no code found in the API.
-    if park_name.lower().find('caroline') > -1: park_code = 'xxx1'
-    if (park_name.lower().find('john d. rockefeller') > -1 or
-        park_name.lower().find('jdrockefeller') > -1): park_code = 'xxx2'
-    if park_name.lower().find('chelan') > -1: park_code = 'xxx3'
-    if park_name.lower().find('ross lake') > -1: park_code = 'xxx4'
-    if park_name.lower().find('valor') > -1: park_code = 'xxx5'
-    if park_name.lower().find('world war i ') > -1: park_code = 'xxx6'
-    if park_name.lower() == "world war i": park_code = 'xxx6'
-    if park_name.lower() == "john f. kennedy center for pa": park_code = 'xxx7'
-
-    # Stripping park name does not work for all data sets for National
-    # Park of American Samoa, so assign directly.
-    if park_name.lower().find('samoa') > -1: park_code = 'npsa'
+    # if park_name.lower() == "arlington house": park_code = 'arho'
+    # if park_name.lower() in ["kings canyon", "sequoia"]: park_code = 'seki'
+    # if park_name.lower() == "white house" : park_code = 'whho'
+    # if park_name.lower() == "fdr": park_code = 'frde'
+    # if park_name.lower() == "gateway nra": park_code = 'gate'
+    # if park_name.lower() == "g w": park_code = 'gwmp'
+    # if park_name.lower() == "martin l king, jr, nhp": park_code = 'malu'
+    # if park_name.lower() == "martin luther king, jr. nhp": park_code = 'malu'
+    # if park_name.lower() == "t roosevelt np": park_code = 'thro'
+    # if park_name.lower().startswith("fred-spots"): park_code = 'frsp'
+    # if park_name.lower().startswith("lbj"): park_code = 'lyba'
+    # if park_name.lower().endswith("wwii"): park_code = 'wwii'
+    # if park_name.lower().find("sumter") > -1: park_code = 'fosu'
+    # if park_name.lower().find("longfellow") > -1: park_code = 'long'
+    #
+    # # These are the parks with no code found in the API.
+    # if park_name.lower().find('caroline') > -1: park_code = 'xxx1'
+    # if (park_name.lower().find('john d. rockefeller') > -1 or
+    #     park_name.lower().find('jdrockefeller') > -1): park_code = 'xxx2'
+    # if park_name.lower().find('chelan') > -1: park_code = 'xxx3'
+    # if park_name.lower().find('ross lake') > -1: park_code = 'xxx4'
+    # if park_name.lower().find('valor') > -1: park_code = 'xxx5'
+    # if park_name.lower().find('world war i ') > -1: park_code = 'xxx6'
+    # if park_name.lower() == "world war i": park_code = 'xxx6'
+    # if park_name.lower() == "john f. kennedy center for pa": park_code = 'xxx7'
+    #
+    # # Stripping park name does not work for all data sets for National
+    # # Park of American Samoa, so assign directly.
+    # if park_name.lower().find('samoa') > -1: park_code = 'npsa'
 
     return park_code
 
@@ -258,18 +276,18 @@ def main():
     df_api = read_park_sites_api()
 
     # Read in the list of nps sites from nps.gov into a dataframe.
-    df_master = read_park_sites_web(df_api)
+    #df_master = read_park_sites_web(df_api)
 
     # Merge the nps.gov and nps api dataframes.
-    df_master = pd.merge(df_master[['park_name', 'park_code', 'designation']],
-                         df_api[['park_code', 'states', 'lat', 'long']],
-                         how='left', on='park_code')
+    #df_master = pd.merge(df_master[['park_name', 'park_code', 'designation']],
+#                         df_api[['park_code', 'states', 'lat', 'long']],
+#                         how='left', on='park_code')
 
     # Read in the date established data from Wikipedia into a
     # dataframe. This data is currently only available for National
     # Parks. Merge this dataframe with the master dataframe.
-    df_estab = read_wikipedia_data(df_api)
-    df_master = pd.merge(df_master, df_estab, how='left', on='park_code')
+    #df_estab = read_wikipedia_data(df_api)
+    #df_master = pd.merge(df_master, df_estab, how='left', on='park_code')
 
     # Read in the park size data from the NPS acreage report downloaded
     # from nps.gov.
@@ -282,12 +300,12 @@ def main():
     #df_master = pd.merge(df_master, df_visitor, how='left', on='park_code')
 
     # Sort and save the master dataframe to an Excel file.
-    df_master = df_master.sort_values(by=['park_name']).reset_index(drop=True)
+    #df_master = df_master.sort_values(by=['park_name']).reset_index(drop=True)
 
-    print("*** DF Master Dupes")
-    print(df_master[df_master.duplicated(['park_code'], keep=False)])
+    #print("*** DF Master Dupes")
+    #print(df_master[df_master.duplicated(['park_code'], keep=False)])
 
-    df_master.to_excel('nps_parks_master_df.xlsx')
+    #df_master.to_excel('nps_parks_master_df.xlsx')
 
 if __name__ == '__main__':
     main()
