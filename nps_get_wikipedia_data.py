f43ab02146bf4429ea4d99084483f5569959ebbc
goodmorningdata <goodmorningdata@gmail.com>
2019-05-30 22:14:19 -0700

    Get established dates for National Monuments from Wikipedia.

--- nps_get_wikipedia_data.py
+++ nps_get_wikipedia_data.py
@@ -27,43 +27,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 
-def get_established_date_from_page(filename):
-    '''
-    This function uses the Python library, BeautifulSoup to extract
-    the park name and date established from a Wikipeida page saved as
-    an html file.
-
-    Parameters
-    ----------
-    filename : str
-        Name of file containing saved Wikipedia page.
-
-    Returns
-    -------
-    df : pandas DataFrame
-        Dataframe of park name and date established.
-    '''
-
-    soup = BeautifulSoup(open(filename), 'html.parser')
-
-    df = pd.DataFrame(columns=['park_name', 'date_established'])
-
-    # Find the table of National Parks.
-    table_rows = soup.find_all('table')[1].find_all('tr')
-
-    # For each row in the table, extract the name of the park, and
-    # the date established and add to the dataframe.
-    for row in table_rows[1:]:
-        name = str(row.a.string)
-        date = str(row.findAll('span', {'data-sort-value' : True})[0].string)
-        date = datetime.strptime(date, '%B %d, %Y')
-        df = df.append({'park_name': name,
-                        'date_established': date
-                       },
-                       ignore_index=True)
-
-    return df
-
 def get_list_of_presidents(filename):
     '''
     This function uses the Python library, BeautifulSoup to extract
@@ -115,14 +78,54 @@ def get_list_of_presidents(filename):
 
     return df
 
-def main():
-    infile = '_reference_data/wikipedia_national_parks.html'
-    df = get_established_date_from_page(infile)
-    df.to_csv('_reference_data/wikipedia_date_established.csv', index=False)
+def get_np_established_date(filename):
+    '''
+    This function uses the Python library, BeautifulSoup to extract
+    the park name and date established from a Wikipeida page saved as
+    an html file.
 
+    Parameters
+    ----------
+    filename : str
+        Name of file containing saved Wikipedia page.
+
+    Returns
+    -------
+    df : pandas DataFrame
+        Dataframe of park name and date established.
+    '''
+
+    soup = BeautifulSoup(open(filename), 'html.parser')
+
+    df = pd.DataFrame(columns=['park_name', 'date_established'])
+
+    # Find the table of National Parks.
+    table_rows = soup.find_all('table')[1].find_all('tr')
+
+    # For each row in the table, extract the name of the park, and
+    # the date established and add to the dataframe.
+    for row in table_rows[1:]:
+        name = str(row.a.string)
+        date = str(row.findAll('span', {'data-sort-value' : True})[0].string)
+        date = datetime.strptime(date, '%B %d, %Y')
+        df = df.append({'park_name': name,
+                        'date_established': date
+                       },
+                       ignore_index=True)
+
+    return df
+
+def main():
     infile = '_reference_data/wikipedia_list_of_presidents.html'
     df = get_list_of_presidents(infile)
     df.to_csv('_reference_data/wikipedia_list_of_presidents.csv', index=False)
+
+    infile = '_reference_data/wikipedia_national_parks.html'
+    df = get_np_established_date(infile)
+    df.to_csv('_reference_data/wikipedia_date_established.csv', index=False)
+
+    infile = '_reference_data/wikipedia_national_monuments.html'
+    df = get_nm_established_date(infile)
 
 if __name__ == '__main__':
     main()
