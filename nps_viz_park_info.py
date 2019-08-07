6ffabfab151900b11026d8b7801d2237161a84ec
goodmorningdata <goodmorningdata@gmail.com>
2019-08-07 08:31:27 -0600

    Add script to display info about a single park.

---
+++ nps_viz_park_info.py
@@ -0,0 +1,33 @@
+'''
+This script creates...
+
+The following visualizations are created:
+1)
+2) Plots including:
+   Plot #1 -
+
+Required Libraries
+------------------
+
+Dependencies
+------------
+1) Run the script, nps_create_master_df.py to create the file,
+   nps_parks_master_df.xlsx.
+'''
+
+from nps_shared import *
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--parkcode', type=str,
+        help = "Four character park code of park for which to display \
+                information. Example values are: 'YELL', 'CUVA', 'SHEN', \
+                etc.")
+    args = parser.parse_args()
+
+    if args.parkcode:
+        print('** Parkcode: {}'.format(args.parkcode))
+
+if __name__ == '__main__':
+    main()
