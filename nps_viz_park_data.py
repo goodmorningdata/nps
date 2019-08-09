d53d20d6d73028f6e6364c75a0d38d1ea3e57513
goodmorningdata <goodmorningdata@gmail.com>
2019-08-09 08:53:10 -0600

    Park info report.

--- nps_viz_park_data.py
+++ nps_viz_park_data.py
@@ -17,6 +17,7 @@ Dependencies
 
 from nps_shared import *
 import argparse
+import sys
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,11 +27,33 @@ def main():
                 etc.")
     args = parser.parse_args()
 
-    if args.parkcode:
-        print('** Parkcode: {}'.format(args.parkcode))
-    else:
+    if not(args.parkcode):
         print("\n** Warning ** ")
         print('Please enter a park code parameter to display park data.\n')
+        sys.exit()
+
+    df = pd.read_excel('nps_parks_master_df.xlsx', header=0)
+    pd.set_option('display.max_columns', None)
+
+    park = df[df.park_code == args.parkcode.lower()]
+    num_stars = len(park.park_name.values[0])
+    stars = '*' * (28 + num_stars)
+    print('\n' + stars)
+    print('Parkcode:                   {}'.format(args.parkcode))
+    print('Park Name:                  {}'.format(park.park_name.values[0]))
+    print('Designation:                {}'.format(park.designation.values[0]))
+    print('States:                     {}'.format(park.states.values[0]))
+    print('Entry Date:                 {}'.format(park.entry_date.values[0]))
+    print('Size (acres):               {:,.2f}'
+          .format(park.gross_area_acres.values[0]))
+    print('Size (square miles):        {:.2f}'
+          .format(park.gross_area_square_miles.values[0]))
+    print('Number of visits in 2018: {}'
+          .format(park[2018].values[0]))
+    print(stars + '\n')
+
+    # Fix date formatting
+    # Add place for size and number of visits
 
 if __name__ == '__main__':
     main()
